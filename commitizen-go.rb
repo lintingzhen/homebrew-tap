3cc89822dcda7da8cf48131e78563ce5996ce108
tingzhen.lin <tingzhen.lin@17zuoye.com>
2020-08-25 23:18:46 +0800

    feat: support configuration how to assemble commit message

--- commitizen-go.rb
+++ commitizen-go.rb
@@ -4,8 +4,8 @@
 class CommitizenGo < Formula
   desc "The commitizen command line utility, without nodejs."
   homepage "https://github.com/lintingzhen/commitizen-go"
-  url "https://github.com/lintingzhen/commitizen-go/releases/download/v0.1.0/commitizen-Darwin-x86-0.1.0.tgz"
-  sha256 "1b4ab6c785e0387e5c9654ca523af72b4a1b4e717a320fdd2f94a58e6ac94dc0"
+  url "https://github.com/lintingzhen/commitizen-go/releases/download/v1.0.0/commitizen-Darwin-x86-1.0.0.tgz"
+  sha256 "db381c47bd8a07e2056a3d0d7affe29bd2d8938b9e4fb8c91dc664c33a0c65f7"
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
